7c7ced88c23440b97e87ed2dd1e92a7ec0f4c9a3
Moundo <drmoundo2@gmail.com>
2026-01-20 19:53:08 +0200

    Update videos.js

--- routes/videos.js
+++ routes/videos.js
@@ -9,11 +9,4 @@ router.get("/", async (req, res) => {
   res.json(data);
 });
 
-router.post("/", async (req, res) => {
-  const data = await fs.readJson(FILE);
-  data.push(req.body);
-  await fs.writeJson(FILE, data, { spaces: 2 });
-  res.json({ success: true });
-});
-
 export default router;
