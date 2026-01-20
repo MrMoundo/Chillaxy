c1313b9f2ac2fd1bffe3710a03e8ca377d1a6ac6
Moundo <drmoundo2@gmail.com>
2026-01-20 22:34:06 +0200

    Update banners.js

--- routes/banners.js
+++ routes/banners.js
@@ -4,20 +4,8 @@ import fs from "fs-extra";
 const router = express.Router();
 const FILE = "./data/banners.json";
 
-router.get("/", async (_,res)=>{
+router.get("/", async (_, res) => {
   res.json(await fs.readJson(FILE));
-});
-
-router.post("/", async (req,res)=>{
-  if(!req.session.user) return res.sendStatus(401);
-
-  const banners = await fs.readJson(FILE);
-  banners.push(req.body.link);
-
-  if (banners.length > 5) banners.shift();
-
-  await fs.writeJson(FILE,banners,{spaces:2});
-  res.json({success:true});
 });
 
 export default router;
