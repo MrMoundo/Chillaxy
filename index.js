c072d86bbd6264e747cdef31c0293585de242d1f
Moundo <drmoundo2@gmail.com>
2026-01-20 20:13:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import videoRoutes from "./routes/videos.js";
+import authRoutes from "./routes/auth.js";
 import "./bot.js";
 
 dotenv.config();
@@ -9,12 +10,9 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/videos", videoRoutes);
+app.use("/auth", authRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Chillaxy Backend is running");
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+app.listen(process.env.PORT || 3000);
