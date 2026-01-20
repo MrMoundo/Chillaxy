ff4a226289bf7d759f3c61377abfc2d21717b366
Moundo <drmoundo2@gmail.com>
2026-01-20 20:26:21 +0200

    Update videos.js

--- routes/videos.js
+++ routes/videos.js
@@ -8,17 +8,11 @@ router.get("/", async (_, res) => {
   res.json(await fs.readJson(FILE));
 });
 
-router.put("/:code", async (req, res) => {
-  const data = await fs.readJson(FILE);
-  const index = data.findIndex(v => v.code === req.params.code);
-  if (index === -1) return res.sendStatus(404);
-
-  data[index] = { ...data[index], ...req.body };
-  await fs.writeJson(FILE, data, { spaces: 2 });
-  res.json({ success: true });
-});
-
 router.delete("/:code", async (req, res) => {
+  if (req.session.user !== process.env.ADMIN_ID) {
+    return res.sendStatus(403);
+  }
+
   const data = await fs.readJson(FILE);
   const filtered = data.filter(v => v.code !== req.params.code);
   await fs.writeJson(FILE, filtered, { spaces: 2 });
