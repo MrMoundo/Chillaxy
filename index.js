cf6302322de7cde917215d53aea439813199e0c9
Moundo <drmoundo2@gmail.com>
2026-01-22 17:32:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 import express from "express";
 import cors from "cors";
-import session from "express-session";
 import dotenv from "dotenv";
+import session from "express-session";
 
-/* ROUTES */
 import authRoutes from "./routes/auth.js";
 import videoRoutes from "./routes/videos.js";
+import adminRoutes from "./routes/admins.js";
 import bannerRoutes from "./routes/banners.js";
 
-/* BOT */
 import "./bot.js";
 
 dotenv.config();
 
 const app = express();
 
-/* ================= MIDDLEWARE ================= */
 app.use(cors());
 app.use(express.json());
 
@@ -23,27 +21,21 @@ app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: false
-    }
+    saveUninitialized: false
   })
 );
 
-/* ================= ROUTES ================= */
 app.use("/auth", authRoutes);
 app.use("/api/videos", videoRoutes);
+app.use("/api/admins", adminRoutes);
 app.use("/api/banners", bannerRoutes);
 
-/* ================= FRONTEND ================= */
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/public/index.html");
 });
 
-/* ================= START SERVER ================= */
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Chillaxy Backend is running");
 });
