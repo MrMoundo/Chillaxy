dfd3f8b807ce0c331a0f4616be3f70b7fcc7a3d8
Moundo <drmoundo2@gmail.com>
2026-01-23 17:36:30 +0200

    Update banners.js

--- routes/banners.js
+++ routes/banners.js
@@ -1,11 +1,25 @@
 import express from "express";
 import fs from "fs-extra";
+import multer from "multer";
 
 const router = express.Router();
 const FILE = "./data/banners.json";
 
+const upload = multer({ dest: "public/banners" });
+
 router.get("/", async (_, res) => {
   res.json(await fs.readJson(FILE));
+});
+
+router.post("/", upload.single("image"), async (req,res)=>{
+  const banners = await fs.readJson(FILE);
+  banners.push({
+    url:`/banners/${req.file.filename}`,
+    time:Date.now()
+  });
+  if(banners.length>5) banners.shift();
+  await fs.writeJson(FILE,banners,{spaces:2});
+  res.json({success:true});
 });
 
 export default router;
